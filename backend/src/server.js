71aa77f6947e9ed4eabdaf01d5130bd08256e560
filipe caldas <filipe.ruas@hotmail.com>
2019-08-11 20:14:41 -0300

    falta corrigir socket que não manda o match

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,31 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
 
-const server = express();
+const httpServer = express();
+const server = require('http').Server(httpServer);
+const io = require('socket.io')(server);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+  const { user } = socket.handshake.query;
+
+  connectedUsers[user] = socket.id;
+})
 
 mongoose.connect('mongodb+srv://tindev:tindev@cluster0-zrava.mongodb.net/tindevdb?retryWrites=true&w=majority', {
   useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+httpServer.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+httpServer.use(cors());
+httpServer.use(express.json());
+httpServer.use(routes);
 
 server.listen(3333);
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import { Link } from 'react-router-dom';
 import './Main.css';
 
@@ -7,9 +8,11 @@ import api from '../services/api';
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
+import itsamatch from '../assets/itsamatch.png';
 
 export default function Main({ match }) {
   const [users, setUsers] = useState([]);
+  const [matchDev, setMatchDev] = useState(null);
   
   useEffect(() => {
     async function loadUsers() {
@@ -23,6 +26,18 @@ export default function Main({ match }) {
     loadUsers();
   }, [match.params.id]);
   
+  useEffect(() => {
+    const socket =  io('http://localhost:3333', {
+      query: { user: match.params.id }
+    });
+
+    socket.on('match', dev => {
+      console.log(dev)
+      setMatchDev(dev);
+    })
+
+  }, [match.params.id])
+
   async function handleLike(id) {
     await api.post(`/devs/${id}/likes`,null, {
       headers: { user: match.params.id }
@@ -72,6 +87,18 @@ export default function Main({ match }) {
       ) : (
         <div className="empty">Acabo :(</div>
       ) }
+
+      { matchDev && (
+        <div className="match-container">
+          <img src={itsamatch} alt="Its a match"/>
+          <img src={matchDev.avatar} alt=""/>
+          <strong>{matchDev.name}</strong>
+          <p>{matchDev.bio}</p>
+
+          <button type="button" onClick={() => setMatchDev(null)}>FECHAR</button>
+        </div>
+      )}
+
     </div>
   );
 }
